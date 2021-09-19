ce8a8c292ec4ab5baba32aec00bee218b9735415
wataflai <anthonygenio2005@gmail.com>
2021-09-19 17:37:43 -0400

    made a slightly cleaner version of the two

---
+++ vigenere-encryption2.py
@@ -0,0 +1,44 @@
+from hashlib import sha1
+
+# encrypts a message given a key
+def encrypt(message: str, key: str) -> str:
+
+    # hash the key to make it more secure
+    key = str(sha1(key.encode()).hexdigest())
+
+    arr = [i for i in message]
+
+    # make a key generator that loops so I can use it for as long as the message
+    key_gen = map(str, key * (len(message) // len(key) + 1))
+
+    # shifts the letters in the message according to the key given
+    for i in range(len(arr)):
+        arr[i] = chr(ord(arr[i]) + ord(next(key_gen)))
+
+    # collapse the array into a string again
+    return ''.join(arr)
+
+
+# encrypts a message given a key
+def decrypt(message: str, key: str) -> str:
+
+    # hash the key to make it more secure
+    key = str(sha1(key.encode()).hexdigest())
+
+    arr = [i for i in message]
+
+    # make a key generator that loops so I can use it for as long as the message
+    key_gen = map(str, key * (len(message) // len(key) + 1))
+
+    # shifts the letters in the message according to the key given
+    for i in range(len(arr)):
+        arr[i] = chr(ord(arr[i]) - ord(next(key_gen)))
+
+    # collapse the array into a string again
+    return ''.join(arr)
+
+
+
+# encrypt = input('Hello! would you like to encrypt (e) or decrypt (d) a message?') == 'e'
+KEY = 'hello there'
+print(encrypt('I am a turtle, a beautiful one too', KEY))
